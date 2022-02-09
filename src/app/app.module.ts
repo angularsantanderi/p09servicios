545e35878c3e9e390000619d4d426223977423d8
angularsantanderi <angularsantanderi@gmail.com>
2022-02-09 09:43:27 +0100

    add servicio y componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TablaClientesComponent } from './clientes/tabla-clientes/tabla-clientes.component';
+import { CrearClienteComponent } from './clientes/crear-cliente/crear-cliente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TablaClientesComponent,
+    CrearClienteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
